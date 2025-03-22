4fb4600b5b8b8c68e61dba88b41b2068360bb587
HaraNagaSai <231FA04456@gmail.com>
2025-03-22 12:55:20 +0530

    Update book.js

--- book.js
+++ book.js
@@ -1,18 +1,13 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const bookSchema = new mongoose.Schema({
-  title: String,
-  ISBN: String,
-  stock: Number,
-  author: String,
-  description: String,
-  category: String,
-  comments: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Comment",
-    },
-  ],
+const bookSchema = new Schema({
+  bookId: { type: Number, required: true, unique: true },
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  available: { type: Boolean, default: true },
+  checkedOutBy: { type: Schema.Types.ObjectId, ref: 'User', default: null },
+  checkoutDate: { type: Date, default: null }
 });
 
-module.exports = mongoose.model("Book", bookSchema);
+module.exports = mongoose.model('Book', bookSchema);
